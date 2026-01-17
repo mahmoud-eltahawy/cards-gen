2b6958113a002891c5fc6792e224e6c990bb64ce
mahmoud-eltahawy <gemmy9300@gmail.com>
2026-01-17 05:54:29 +0200

    first commit

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,48 @@
+use serde::{Deserialize, Serialize};
+use specta::Type;
+use specta_typescript::Typescript;
+use tauri_specta::{Builder, collect_commands};
+
+#[derive(Serialize, Type)]
+struct GreetReturn {
+    inner: String,
+}
+
+#[derive(Deserialize, Type)]
+struct GreetArgs {
+    name: String,
+}
+
+#[tauri::command]
+#[specta::specta]
+fn greet(args: GreetArgs) -> GreetReturn {
+    let GreetArgs { name } = args;
+    GreetReturn {
+        inner: format!("Hello, {}! You've been greeted from Rust!", name),
+    }
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    let builder = Builder::new().commands(collect_commands![greet]);
+
+    #[cfg(debug_assertions)]
+    builder
+        .export(
+            Typescript::default()
+                .formatter(specta_typescript::formatter::prettier)
+                .header("/* eslint-disable */"),
+            "../src/tauri_bindings.ts",
+        )
+        .expect("Failed to export typescript bindings");
+
+    tauri::Builder::default()
+        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(builder.invoke_handler())
+        .setup(move |app| {
+            builder.mount_events(app);
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
